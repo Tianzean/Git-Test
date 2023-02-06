483a37781a9e8ca391629e092096edd46f1103da
Antianze <antianze98@gmail.com>
2023-02-06 20:46:37 +0000

    first commit

---
+++ src/main/java/Git_Test.java
@@ -0,0 +1,9 @@
+/**
+ * @author Tianze
+ * @create 2023-02-06-20:44
+ **/
+public class Git_Test {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
